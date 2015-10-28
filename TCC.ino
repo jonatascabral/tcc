346df9eb19a53cc8f6ac17ee8bdccfccde683faf
Jonatas Cabral <jon-jons@hotmail.com>
2015-10-28 15:36:23 -0200

    Alterações e correções, novo datasheet para o display

--- TCC.ino
+++ TCC.ino
@@ -20,20 +20,18 @@
  * SPI MISO    MISO         12 / ICSP-1   50        D12        ICSP-1           14
  * SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15
  */
-int pinReset = 0,               // RFID RST
-    pinSDA = 1,                 // RFID SDA(SS)
+int pinReset = 49,               // RFID RST
+    pinSDA = 53,                 // RFID SDA(SS)
 
-    pinDirecao = 7,             // Servo
-    pinLed = 8,                 // Led de alertas
+    pinDirecao = 2,             // Servo
+    pinLed = 17,                 // Led de alertas
 
     pinFrente = 3,              // OUT 3
-    pinTras = 6,                // OUT 4
+    pinTras = 4;                // OUT 4
 
-    pinDireita = 4,             // OUT 1
-    pinEsquerda = 5;            // OUT 2
 uint16_t maxVelocidade = 50,         // Velocidade maxima ao ler a tag @TODO: definir velocidades para cada tag/cartao lido
-         velocidadeDefault = 135,    // Velocidade maxima para economizar bateria
-         velocidadeMotor = 135;      // Velocidade do motor da direcao
+         velocidadeDefault = 125,    // Velocidade maxima para economizar bateria
+         velocidadeDefaultBack = 60;
 
 bool limitaVelocidade = false,  // Define se deve limitar a velocidade
      aceso = false;
@@ -57,49 +55,25 @@ USB Usb;
  * Conecte o adaptador novamente;
  * Espere sincronizar;
  */
-//BTD Btd(&Usb);
-//PS3BT PS3(&Btd);
+// BTD Btd(&Usb);
+// PS3BT PS3(&Btd);
 
 // PS3 Cable mode
 PS3USB PS3(&Usb); // Caso use a versao bluetooth comente esta linha
 
 
-// void virar(int graus) {
-//     if (graus < 15) {
-//         graus = 15;
-//     }
-//     servoMotor.write(graus);
-// }
-
-void virar(uint16_t forca, int direcao = 0) {
-    switch (direcao) {
-        case 1: // esquerda
-            analogWrite(pinEsquerda, forca);
-        break;
-        case 2: // direita
-            analogWrite(pinDireita, forca);
-        break;
-        case 3: // analogico, compara pra saber a direcao
-            // int pinUsado;
-            // if (forca > 127) {
-            //     pinUsado = pinEsquerda;
-            // } else {
-            //     pinUsado = pinDireita;
-            // }
-            // analogWrite(pinUsado, forca);
-        break;
-        default:
-            analogWrite(pinEsquerda, 0);
-            analogWrite(pinDireita,  0);
-        break;
+void virar(int graus) {
+    if (graus < 15) {
+        graus = 15;
     }
+    servoMotor.write(graus);
 }
 
 void acelerar(uint16_t velocidade, bool forward = true) {
     if (velocidade > velocidadeDefault && forward) {
         velocidade = velocidadeDefault;
-    } else if (velocidade > velocidadeDefault && !forward) {
-        velocidade = 70;
+    } else if (velocidade > velocidadeDefaultBack && !forward) {
+        velocidade = velocidadeDefaultBack;
     }
     if (limitaVelocidade && forward && velocidade > maxVelocidade) {
         velocidade = maxVelocidade;
@@ -123,7 +97,9 @@ void blink() {
 
 
 void setup() {
+    Serial.begin(9600);
     if (Usb.Init() == -1) {
+        Serial.write("Erro ao iniciar driver USB");
         while(1);
     }
 
@@ -131,17 +107,16 @@ void setup() {
     SPI.begin();
     mfrc522.PCD_Init();
 
-    // servoMotor.attach(pinDirecao);
+    servoMotor.attach(pinDirecao);
     pinMode(pinLed,      OUTPUT);
 
     pinMode(pinFrente,   OUTPUT);
     pinMode(pinTras,     OUTPUT);
 
-    pinMode(pinDireita,  OUTPUT);
-    pinMode(pinEsquerda, OUTPUT);
-
     // Blink Led, app start OK
     blink();
+
+    Serial.write("Setup finalizado");
 }
 
 void loop() {
@@ -165,21 +140,20 @@ void loop() {
 
         // Virar direcionais
         if (PS3.getButtonPress(LEFT)) {
-            // virar(map(0, 0, 255, 0, 180));
-            virar(velocidadeMotor, 1);
-            delay(80);
+            virar(map(0, 0, 255, 0, 180));
+            // virar(velocidadeMotor, 1);
+            // delay(80);
         } else if (PS3.getButtonPress(RIGHT)) {
-            // virar(map(255, 0, 255, 0, 180));
-            virar(velocidadeMotor, 2);
-            delay(80);
+            virar(map(255, 0, 255, 0, 180));
+            // virar(velocidadeMotor, 2);
+            // delay(80);
         } else {
-            virar(0);
+            // Virar Analogico esquerdo
+            uint16_t leftAnalog = PS3.getAnalogHat(LeftHatX);
+            virar(map(leftAnalog, 0, 255, 0, 180));
+            // virar(leftAnalog, 3);
         }
 
-        // Virar Analogico esquerdo
-        uint16_t leftAnalog = PS3.getAnalogHat(LeftHatX);
-        // virar(map(leftAnalog, 0, 255, 0, 180));
-        virar(leftAnalog, 3);
 
         if (mfrc522.PICC_IsNewCardPresent() || mfrc522.PICC_ReadCardSerial()) {
             limitaVelocidade = !limitaVelocidade;
